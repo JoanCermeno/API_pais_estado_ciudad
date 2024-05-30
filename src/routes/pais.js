9c00169bb13dc02719df3227c867badf9580cbe6
Joan Gabriel <joangabriel684@gmail.com>
2024-05-30 13:11:22 -0400

    /pais bug fix queryParams

--- src/routes/pais.js
+++ src/routes/pais.js
@@ -3,34 +3,26 @@ const router = express.Router();
 const Pais = require("../models/Pais");
 const isString = require("../../lib/isString");
 
-router.get("/", async (req, res) => {
+router.get("/:idPais", async (req, res) => {
   if (req.secure) {
     req.protocol = "https";
   }
-  //recibiendo parámetros de búsqueda
-  const { nombre, id } = req.query;
+
+  const { idPais } = req.params;
+
+  //consulta a la base de datos
   const Query = Pais.query();
 
-  if (id) {
-    const parsedId = parseInt(id, 10); // Parsear el ID a un número entero
+  if (idPais) {
+    const parsedId = parseInt(idPais, 10); // Parsear el ID a un número entero
     if (isNaN(parsedId) || parsedId <= 0 || parsedId > 1000) {
-      return res
-        .status(400)
-        .send({
-          error:
-            'El parámetro "id" debe ser un número entero y no mayor a 1000.',
-        });
+      //validando id
+      return res.status(400).send({
+        error: true,
+        mensaje: `El parámetro "id" debe ser un número entero y no mayor a 1000.`,
+      });
     }
     Query.where("id", parsedId);
-  } else {
-    if (nombre == "undefined") {
-      return res
-        .status(400)
-        .send({ error: 'El parámetro "nombre" no puede tener undefined' });
-    }
-    if (typeof nombre != "undefined" && isString(nombre)) {
-      Query.where("name", "LIKE", `%${nombre}%`);
-    }
   }
 
   try {
@@ -41,4 +33,26 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/", async (req, res) => {
+  //recibiendo parámetros de búsqueda
+  const { nombre } = req.query;
+  console.log("Entre en juego");
+  //consulta a la base de datos
+  try {
+    if (nombre == "undefined") {
+      return res
+        .status(400)
+        .send({ error: 'El parámetro "nombre" no puede tener undefined' });
+    }
+    if (typeof nombre != "undefined" && isString(nombre)) {
+      const paisNamed = await Pais.query().where("name", "LIKE", `%${nombre}%`);
+      return res.send(paisNamed);
+    }
+    const paises = await Pais.query();
+    return res.send(paises);
+  } catch (error) {
+    console.log(error);
+    res.send({ mensaje: "Ocurrio un error al buscar el pais", error: true });
+  }
+});
 module.exports = router;
